3af75f883463c8076b5fe0be8b210dafeebe6433
nikita09-lab <nikitamishra090906@gmail.com>
2026-02-22 16:31:41 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
-    baseURL: API_BASE_URL,
-    timeout: 120000,
+  baseURL: `${API_BASE_URL}/api/v1`,
+  timeout: 120000,
 });
 
 /**
